dade6a1b83623385ae91ee4ee90b1550e0dbd3d6
basantbht <bhattbasant012@gmail.com>
2026-04-06 03:29:25 +0545

    vercel.json

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Rent-the-Fit
 Clothes rental website
+
+# frontend 
+npm start
+
+# backend 
+npm start
--- backends/DB/connectDB.js
+++ backends/DB/connectDB.js
@@ -1,15 +1,31 @@
+// const mongoose = require("mongoose");
+
+// const connectDB = async (DATABASE_URI) => {
+// try {
+  
+//     await mongoose.connect(DATABASE_URI);
+//     console.log("Connected to DataBase");
+//   }
+
+// catch (e) {
+//   console.log(e);
+// }
+// }
+// module.exports=connectDB;
+
 const mongoose = require("mongoose");
+const dns = require("dns");
+
+// Force Google DNS - fixes Windows SRV resolution issues
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 const connectDB = async (DATABASE_URI) => {
-try {
-  
-    await mongoose.connect(DATABASE_URI);
-    console.log("Connected to DataBase");
+  try {
+    await mongoose.connect(DATABASE_URI, { family: 4 });
+    console.log("Connected to Database");
+  } catch (e) {
+    console.error("Database connection failed:", e);
   }
+};
 
-catch (e) {
-  console.log(e);
-}
-}
-module.exports=connectDB;
-
+module.exports = connectDB;
